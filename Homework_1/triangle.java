b2d9809062ec9d16446dc244f8d36b4b6e35bdd2
Kagan Ozturkoglu <ozturkoglukagan@gmail.com>
2021-10-18 02:02:08 +0300

    small updates

--- Homework_1/triangle.java
+++ Homework_1/triangle.java
@@ -3,10 +3,10 @@ package Homework_1;
 public class triangle {
 
     public static void main(String[] args) {
-        
+
         // to check if there are too many arguments.
         if (args.length != 1) {
-            System.out.println("To much or to less arguments. Please try again.");
+            System.out.println("Too much or too less arguments. Please try again.");
             // return to finish reading
             return;
         }
@@ -15,30 +15,30 @@ public class triangle {
     }
 
     public static void triangle(int length) {
-            
-            //to check if the number is positive
-            if (length<=0) {
-                System.out.println("The lenght can neither be negative nor zero. Please try again.");
-                return;
-            }
-            /*
-            * drawing the triangle
-            */
-            
-            //to adjust the height of triangle
-            for (int i = 0; i < length; i++) {
-                //to print the row part
-                for (int j = 0; j <i+1; j++) {
-                    //prints the stars
-                    System.out.print("*");
-                
-                }
-                //using println to swith to the new row
-                System.out.println();
-            }
-        
-            //calculating and printing the area by using right triangle formula
-            System.out.println("Area is: "+((double) length*length/2));
 
+        // to check if the number is positive
+        if (length <= 0) {
+            System.out.println("The lenght can neither be negative nor zero. Please try again.");
+            return;
         }
+        /*
+         * drawing the triangle
+         */
+
+        // to adjust the height of triangle
+        for (int i = 0; i < length; i++) {
+            // to print the row part
+            for (int j = 0; j < i + 1; j++) {
+                // prints the stars
+                System.out.print("*");
+
+            }
+            // using println to swith to the new row
+            System.out.println();
+        }
+
+        // calculating and printing the area by using right triangle formula
+        System.out.println("Area is: " + ((double) length * length / 2));
+
+    }
 }
